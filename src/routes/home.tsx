d9340b423ae40c846750ee1f12dbac4b3b9878ec
Anand Agarwal <anand@lypumsolutions.com>
2021-03-14 01:25:23 +0530

    removed warnings

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -4,12 +4,10 @@ import { useHistory } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-import Typography from '@material-ui/core/Typography';
+//import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import Link from '@material-ui/core/Link';
 
 import { AuthContext } from '../contexts/authContext';
 
